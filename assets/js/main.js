76088b4515b7cf62a57746e915aed20636a46421
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-14 16:07:32 +0100

    Add js to create bot button behavior

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,46 +23,74 @@ const images = [
   "./assets/img/05.webp",
 ];
 // definisco le variabili dove mettere le immagini nella dom
-const imagesElement = document.querySelector('.images')
+const imagesElement = document.querySelector(".images");
 console.log(imagesElement);
 
-let activeImage = 0
+let activeImage = 0;
 
 // definisco ciclo for per inserire le immagini una per volta dentro la dom
 for (let i = 0; i < images.length; i++) {
   const imgSrc = images[i];
-  const activebehavior = i === activeImage ? 'active' : ''
-  const imgElement = `<img class="img-fluid ${activebehavior}" src="${imgSrc}" alt="">`
+  const activebehavior = i === activeImage ? "active" : "";
+  const imgElement = `<img class="img-fluid ${activebehavior}" src="${imgSrc}" alt="">`;
   console.log(imgElement);
   // InsertAjacentHTML
-  imagesElement.insertAdjacentHTML('beforeend', imgElement)
+  imagesElement.insertAdjacentHTML("beforeend", imgElement);
 }
 
-// seleziono tutte le img per potergli dare l'active dopo 
-const slideImagesElements = document.querySelectorAll('img')  
+// PULSANTE UP
+// seleziono tutte le img per potergli dare l'active dopo
+const slideImagesElements = document.querySelectorAll("img");
 // seleziono il pulsante UP e creo una variabile
-const nextEl = document.querySelector('.position_up')
+const nextEl = document.querySelector(".position_up");
+
 // ascolto il pulsante UP
-nextEl.addEventListener('click', function () {
-  console.log('cliccato up');
+nextEl.addEventListener("click", function () {
+  console.log("cliccato up");
   console.log(slideImagesElements); //array[index]
-  
+
   // seleziono la slide corrente
-  const currentSlide = slideImagesElements[activeImage]
+  const currentSlide = slideImagesElements[activeImage];
   console.log(currentSlide);
-  
+
   // rimuovo dalla slide corrente la classe active
-  currentSlide.classList.remove('active')
+  currentSlide.classList.remove("active");
 
   // incremento il valore della variabile nel ciclo for sopra
-  activeImage++
+  activeImage++;
+  console.log(activeImage);
 
   // seleziono la prossima immagine
-  console.log(activeImage);
-  const nextImage = slideImagesElements[activeImage]
+  const nextImage = slideImagesElements[activeImage];
 
   // e le aggiungo la classe active
   console.log(nextImage);
-  nextImage.classList.add('active')
+  nextImage.classList.add("active");
+});
+// PULSANTE BOT
+const prevEl = document.querySelector(".position_bot");
 
-})
+// ascolto il pulsante bot
+prevEl.addEventListener("click", function () {
+  console.log("cliccato prev");
+
+  console.log(slideImagesElements); //array[index]
+
+  // seleziono la slide corrente
+  const currentSlide = slideImagesElements[activeImage];
+  console.log(currentSlide);
+
+  // rimuovo dalla slide corrente la classe active
+  currentSlide.classList.remove("active");
+
+  // riduco il valore della variabile nel ciclo for sopra
+  activeImage--;
+  console.log(activeImage);
+
+  // seleziono la prossima immagine
+  const nextImage = slideImagesElements[activeImage];
+  
+  // aggiungo la classe active alla prossima immagine
+  console.log(nextImage);
+  nextImage.classList.add("active");
+});
